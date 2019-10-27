3dd9950c13d1fcdb647284fb27477164bfe22527
PhotonQuantum <cy.n01@outlook.com>
2019-10-27 20:19:55 +0800

    add device status monitor logic

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,5 +1,52 @@
 <template>
-<v-container>
-<p>This is summary page.</p>
-</v-container>
+  <v-container>
+    <p>This is summary page.</p>
+    <p>online: {{ online }} offline: {{ offline }} lost: {{ lost }}</p>
+    <p>{{ all_devs }}</p>
+  </v-container>
 </template>
+<script>
+import PouchDB from "pouchdb-browser";
+export default {
+  data: () => ({
+    timenow: 0,
+    summary: []
+  }),
+  pouch: {
+    summary: {}
+  },
+  computed: {
+    all_devs: function() {
+      return this.summary.filter(x => x.device != undefined).map(x => x.device);
+    },
+    online: function() {
+      return this.summary.filter(
+        x => x.status != "offline" && x.time > this.timenow - 10
+      ).length;
+    },
+    lost: function() {
+      return this.summary.filter(
+        x => x.status != "offline" && x.time <= this.timenow - 10
+      ).length;
+    },
+    offline: function() {
+      return this.summary.filter(x => x.status == "offline").length;
+    }
+  },
+  methods: {
+    updatetime: function() {
+      this.timenow = new Date() / 1000;
+    }
+  },
+  created: function() {
+    this.updatetime();
+    this.$pouch.pull("summary", "http://localhost:5984/summary", {
+      live: true
+    });
+    setInterval(this.updatetime, 5000);
+  },
+  destroyed: function() {
+    this.$pouch.close("summary");
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.config.productionTip = false
 
 Vue.use(PouchVue, {
   pouch: PouchDB,    // optional if `PouchDB` is available on the global object
-  defaultDB: 'summary',  // this is used as a default connect/disconnect database
+  // defaultDB: 'summary',  // this is used as a default connect/disconnect database
   optionDB: {}, // this is used to include a custom fetch() method (see TypeScript example)
   // debug: '*' // optional - See `https://pouchdb.com/api.html#debug_mode` for valid settings (will be a separate Plugin in PouchDB 7.0)
 });
