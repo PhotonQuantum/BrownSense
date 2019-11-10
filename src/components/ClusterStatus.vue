ac701190e9d324903ceedf69b74e27db5473783a
PhotonQuantum <cy.n01@outlook.com>
2019-11-10 16:10:51 +0800

    add summary chart

---
+++ src/components/ClusterStatus.vue
@@ -0,0 +1,28 @@
+<template>
+<doughnut-chart :chart-data="datacollection" :options="chart_options"></doughnut-chart>
+</template>
+<script>
+import DoughnutChart from './DoughnutChart.js';
+export default {
+    data: ()=>({
+        chart_options:{cutoutPercentage: 70}
+    }),
+    props:["online", "offline", "lost"],
+    components:{DoughnutChart},
+    computed: {
+    datacollection: function() {
+        return {
+            datasets: [{
+                data: [this.online, this.offline, this.lost],
+                backgroundColor: ["#36a2eb", "#ffcc56", "#fe6383"]
+            }],
+            labels: [
+                'online',
+                'offline',
+                'lost'
+            ]
+        }
+    },
+}
+}
+</script>
--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,17 +1,48 @@
 <template>
   <v-container>
-    <p>This is summary page.</p>
-    <p>online: {{ online }} offline: {{ offline }} lost: {{ lost }}</p>
-    <p>{{ all_devs }}</p>
+    <v-layout text-center wrap>
+      <v-flex mb-4>
+        <p>This is summary page.</p>
+        <p>{{ all_devs }}</p>
+        <v-row justify="center">
+          <v-col cols="12" md="4">
+            <v-card>
+              <v-card-title class="text-uppercase">
+                <span class="font-weight-light">cluster status</span>
+              </v-card-title>
+              <div class="graph">
+                <cluster-status :online="online" :offline="offline" :lost="lost"></cluster-status>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="4">
+            <v-card>
+              <v-card-title class="text-uppercase">
+                <span class="font-weight-light">actuator status</span>
+              </v-card-title>
+              <div class="graph">
+                <actuator-status :working="working" :pending="pending"></actuator-status>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 <script>
 import PouchDB from "pouchdb-browser";
+import ClusterStatus from "./ClusterStatus";
+import ActuatorStatus from "./ActuatorStatus";
 export default {
   data: () => ({
     timenow: 0,
     summary: []
   }),
+  components: {
+    "cluster-status": ClusterStatus,
+    "actuator-status": ActuatorStatus
+  },
   pouch: {
     summary: {}
   },
@@ -31,6 +62,17 @@ export default {
     },
     offline: function() {
       return this.summary.filter(x => x.status == "offline").length;
+    },
+    working: function() {
+      return this.summary.filter(
+        x =>
+          x.status != "offline" &&
+          x.time > this.timenow - 10 &&
+          x.actuator == true
+      ).length;
+    },
+    pending: function() {
+      return this.online - this.working;
     }
   },
   methods: {
@@ -50,3 +92,10 @@ export default {
   }
 };
 </script>
+<style scoped>
+.graph {
+  width: 300px;
+  margin: 0 auto;
+  padding-bottom: 20px;
+}
+</style>
