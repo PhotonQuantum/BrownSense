a064db99c642f2398e556a0fc3de7f055aeee7fa
刘涵之(LiuHanZhi) <55851864+MisakaCenter@users.noreply.github.com>
2019-12-07 19:59:42 +0800

    feat: beautify 404 page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,17 @@
 <template>
     <v-container>
-    Not Found</v-container>
+		<v-alert type="error" class="mx-2">
+			Page not found.
+		</v-alert>
+	</v-container>
 </template>
 
 <script>
     export default {
-        name: "NotFound"
+        name: "NotFound",
+		created() {
+            this.$store.commit("set_nav", [{text: "404", disabled: true}]);
+        }
     }
 </script>
 
